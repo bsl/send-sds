e7ce1a123462254b3e5de78a2604148279c017e2
brian sorahan <bsorahan@gmail.com>
2013-09-01 22:05:53 -0500

    reformatting

--- main.c
+++ main.c
@@ -106,22 +106,12 @@ end:
 
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
-static void
-display_usage(void)
-{
-    fprintf(
-        stderr,
-        "send-sds " VERSION "\n"
-        "usage: <alsa-device> <channel-num> <sample-num> <sds-filename>\n"
-    );
+static void display_usage(void) {
+    fprintf(stderr, "send-sds " VERSION "\n"
+            "usage: <alsa-device> <channel-num> <sample-num> <sds-filename>\n");
 }
 
-static int
-convert_channel_num(
-    char *s,
-    unsigned int *channel_num,
-    err_t err
-) {
+static int convert_channel_num(char *s, unsigned int *channel_num, err_t err) {
     unsigned int ui;
 
     if (!convert_string_to_unsigned_int(s, &ui)) {
@@ -137,12 +127,7 @@ convert_channel_num(
     return 1;
 }
 
-static int
-convert_sample_num(
-    char *s,
-    unsigned int *sample_num,
-    err_t err
-) {
+static int convert_sample_num(char *s, unsigned int *sample_num, err_t err) {
     unsigned int ui;
 
     if (!convert_string_to_unsigned_int(s, &ui)) {
@@ -158,11 +143,7 @@ convert_sample_num(
     return 1;
 }
 
-static int
-convert_string_to_unsigned_int(
-    char *s,
-    unsigned int *ui
-) {
+static int convert_string_to_unsigned_int(char *s, unsigned int *ui) {
     char *endptr;
     unsigned int c;
 
@@ -183,15 +164,9 @@ convert_string_to_unsigned_int(
 
 #define max(a,b) ((a) > (b) ? (a) : (b))
 
-static int
-send_file(
-    int fd,
-    size_t file_size,
-    midi_t midi,
-    unsigned int channel_num,
-    unsigned int sample_num,
-    err_t err
-) {
+static int send_file(int fd, size_t file_size, midi_t midi,
+                     unsigned int channel_num, unsigned int sample_num,
+                     err_t err) {
     unsigned char buf[max(SDS_HEADER_LENGTH, SDS_PACKET_LENGTH)];
     response_t response;
     unsigned int num_packets, packet_num, modded_packet_num;
@@ -240,18 +215,17 @@ send_file(
             return 0;
         }
 
-        if (!get_response(midi, channel_num, modded_packet_num, &response, err)) {
+        if (!get_response(midi, channel_num,
+                          modded_packet_num, &response, err)) {
             return 0;
         }
 
         if (response != RESPONSE_ACK) {
-            err_set2(
-                err,
-                "received %s instead of %s in response to packet %d",
-                response_to_string(response),
-                response_to_string(RESPONSE_ACK),
-                packet_num
-            );
+            err_set2(err,
+                     "received %s instead of %s in response to packet %d",
+                     response_to_string(response),
+                     response_to_string(RESPONSE_ACK),
+                     packet_num);
             return 0;
         }
 
@@ -261,14 +235,9 @@ send_file(
     return 1;
 }
 
-static int
-get_response(
-    midi_t midi,
-    unsigned int channel_num,
-    unsigned int modded_packet_num,
-    response_t *response,
-    err_t err
-) {
+static int get_response(midi_t midi, unsigned int channel_num,
+                        unsigned int modded_packet_num,
+                        response_t *response, err_t err) {
     int done;
     unsigned char c, x;
     response_state_t state;
@@ -283,34 +252,24 @@ get_response(
 
         switch (state) {
             case STATE0:
-                state = (c == 0xf0)
-                    ? STATE1
-                    : STATE0;
+                state = (c == 0xf0) ? STATE1 : STATE0;
                 break;
 
             case STATE1:
-                state = (c == 0x7e)
-                    ? STATE2
-                    : STATE0;
+                state = (c == 0x7e) ? STATE2 : STATE0;
                 break;
 
             case STATE2:
-                state = (c == channel_num)
-                    ? STATE3
-                    : STATE0;
+                state = (c == channel_num) ? STATE3 : STATE0;
                 break;
 
             case STATE3:
-                state = (c >= 0x7c && c <= 0x7f)
-                    ? STATE4
-                    : STATE0;
+                state = (c >= 0x7c && c <= 0x7f) ? STATE4 : STATE0;
                 x = c;
                 break;
 
             case STATE4:
-                state = (c == modded_packet_num)
-                    ? STATE5
-                    : STATE0;
+                state = (c == modded_packet_num) ? STATE5 : STATE0;
                 break;
 
             case STATE5:
@@ -335,9 +294,7 @@ get_response(
     return 0;
 }
 
-static const char *
-response_to_string(response_t response)
-{
+static const char * response_to_string(response_t response) {
     switch (response) {
         case RESPONSE_ACK:    return "ACK";
         case RESPONSE_NAK:    return "NAK";
