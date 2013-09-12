7b16d01f783595b3945df2cd3ef864fccac403ed
brian sorahan <bsorahan@gmail.com>
2013-09-11 22:23:09 -0500

    added more info to dump command

--- src/midisds_dump.c
+++ src/midisds_dump.c
@@ -8,8 +8,28 @@
 // returns an exit code
 int midisds_dump(const char *filename) {
     SF_INFO sfinfo;
-    sf_open(filename, SFM_READ, &sfinfo);
+    SNDFILE *sndfile;
+
+    sndfile = sf_open(filename, SFM_READ, &sfinfo);
     printf("%-20s %s\n", "file", filename);
     printf("%-20s %d\n", "frames", (int) sfinfo.frames);
+    printf("%-20s %d\n", "samplerate", sfinfo.samplerate);
+    printf("%-20s %d\n", "channels", sfinfo.channels);
+
+    SF_FORMAT_INFO fmtinfo;
+    fmtinfo.format = (int) sfinfo.format;
+    if (sf_command(sndfile, SFC_GET_FORMAT_INFO,
+                   &fmtinfo, sizeof(fmtinfo)) == 0) {
+        printf("%-20s %s\n", "format", fmtinfo.name);
+    } else {
+        printf("%s\n", sf_strerror(sndfile));
+        return 4;
+    }
+
+    printf("%-20s %d\n", "sections", sfinfo.sections);
+    printf("%-20s %s\n", "seekable", sfinfo.seekable ? "yes" : "no");
+
+    sf_close(sndfile);
     return 0;
 }
+
