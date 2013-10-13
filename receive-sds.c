b7540db329b9559668a510e17dbb70eac8b0b971
Brian Sorahan <bsorahan@gmail.com>
2013-10-13 15:11:32 -0500

    Receive is working. Need to make it write to a file.

--- receive-sds.c
+++ receive-sds.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <time.h>
 
 #include "common.h"
 #include "err.h"
@@ -21,26 +22,6 @@
 #define __TRACE_READ_PACKET_WAIT 1
 
 #define SDS_RESPONSE_LENGTH 6
-
-
-
-typedef enum {
-    HEADER_STATE0,  /* seen [],                   hoping for f0 */
-    HEADER_STATE1,  /* seen [f0]                  hoping for 7e */
-    HEADER_STATE2,  /* seen [f0,7e]               hoping for channel number */
-    HEADER_STATE3,  /* seen [f0,7e,CN]            hoping for 01,... */
-    HEADER_STATE4   /* seen [f0,7e,CN,01,...]     hoping for f7 */
-} receive_header_state_t;
-
-typedef enum {
-    PACKET_STATE0,  /* seen [],                   hoping for f0 */
-    PACKET_STATE1,  /* seen [f0]                  hoping for 7e */
-    PACKET_STATE2,  /* seen [f0,7e]               hoping for channel number */
-    PACKET_STATE3,  /* seen [f0,7e,CN]            hoping for 01,... */
-    PACKET_STATE4,  /* seen [f0,7e,CN,02,]        hoping for packet number */
-    PACKET_STATE5,  /* seen [f0,7e,CN,02,...]     reading waveform data, hoping for ll */
-    PACKET_STATE6   /* seen [f0,7e,CN,02,...,ll]  hoping for f7 */
-} receive_packet_state_t;
 
 
 
@@ -94,7 +75,7 @@ int main(int argc, char **argv) {
 
     if (argc != 1 + 4) {
         display_usage();
-        goto end;
+        exit(249);
     }
 
     device = argv[1];
@@ -107,12 +88,9 @@ int main(int argc, char **argv) {
         ! midi_open_interface(device, &midi, err)                ||
         ! sds_open_file(filename, &fd, err)                      ||
         ! receive_sample(fd, midi, sample_num, channel_num, err)) {
-
-        fprintf(stderr, "%s\n", err_get(err));
-        goto end;
+        fprintf(stderr, "Could not receive sample.\n");
     }
 
-end:
     err_destroy(err);
 
     if (fd) {
@@ -140,30 +118,33 @@ receive_sample(int fd,
                unsigned int sample_num,
                unsigned int channel_num,
                err_t err) {
-    unsigned int num_packets, packet_num, modded_packet_num;
-
-    packet_num = num_packets = modded_packet_num = 0;
+    unsigned int packet_num;
+    packet_num = 0;
 
     printf("Initiating handshake.\n");
 
     if (! read_dump_header(midi, sample_num, channel_num, err)) {
         return 0;
     } else {
-        printf("Sending ACK\n");
-        if (send_response(midi, channel_num, modded_packet_num, RESPONSE_ACK, err)) {
+        printf("Sending ACK...\n");
+        if (send_response(midi, channel_num, packet_num, RESPONSE_ACK, err)) {
             printf("Handshake successful. Begin closeloop transfer.\n");
         }
     }
 
-    int readingPackets = 1;
+    int readingPackets, packet_size;
+    readingPackets = 1;
+
     while (readingPackets) {
-        if (! read_packet(midi, sample_num, channel_num, modded_packet_num, err)) {
+        packet_size = read_packet(midi, sample_num, channel_num, packet_num, err);
+        if (packet_size != SDS_PACKET_LENGTH) {
+            fprintf(stderr, "Received bad packet size %d\n", packet_size);
             readingPackets = 0;
             return 0;
         } else {
-            printf("Sending ACK\n");
-            if (send_response(midi, channel_num, modded_packet_num, RESPONSE_ACK, err)) {
-                modded_packet_num = ++packet_num % 128;
+            printf("Sending ACK...\n");
+            if (send_response(midi, channel_num, packet_num, RESPONSE_ACK, err)) {
+                packet_num = ++packet_num % 128;
             }
         }
     }
@@ -176,242 +157,147 @@ read_dump_header(midi_t midi,
                  unsigned int sample_num,
                  unsigned int channel_num,
                  err_t err) {
-    const char *indent = "    ";
-    const char *trace = "[TRACE]";
-
-    int done, bytes_read, i;
+    int done, i;
+    int bytes_read;
     unsigned char c;
     unsigned char buf[READ_PACKET_BUF_SIZE];
-    receive_header_state_t state;
+    char strbuf[200]; strbuf[0] = '\0';
 
     done = bytes_read = 0;
-    state = HEADER_STATE0;
 
-    printf("%s%s Reading Dump Header\n", indent, trace);
+    printf("Reading Dump Header\n");
 
-    while (! done) {
-        if (bytes_read == READ_PACKET_BUF_SIZE) {
-            char strbuf[200]; strbuf[0] = '\0';
-            sds_serialize_header(strbuf, buf);
-
-            printf("%s%s Read Dump Header %s", indent, trace, strbuf);
-            printf("\nMaximum buffer size reached (%d)\n", READ_PACKET_BUF_SIZE);
-            break;
-        }
-
+    while (bytes_read < SDS_HEADER_LENGTH) {
         if (! midi_read(midi, &c)) {
             err_set2(err, "Could not read midi");
             return 0;
-        } else {
         }
 
-        switch(state) {
-        case HEADER_STATE0:
+        switch(bytes_read) {
+        case 0:
             if (c == 0xf0) {
-                state = HEADER_STATE1;
                 buf[bytes_read++] = c;
-
-                if (__TRACE_READ_DUMP_HEADER && __TRACE_READ_DUMP_HEADER_WAIT) {
-                    printf("%s%s Read first byte of dump header\n", indent, trace);
-                }
             }
             break;
-        case HEADER_STATE1:
+        case 1:
             if (c == 0x7e) {
-                state = HEADER_STATE2;
                 buf[bytes_read++] = c;
-
-                if (__TRACE_READ_DUMP_HEADER) {
-                    printf("%s%s Read second byte of dump header\n", indent, trace);
-                }
             }
             break;
-        case HEADER_STATE2:
+        case 2:
             if (c == channel_num) {
-                state = HEADER_STATE3;
                 buf[bytes_read++] = c;
-
-                if (__TRACE_READ_DUMP_HEADER) {
-                    printf("%s%s Read channel num %d from dump header\n",
-                           indent, trace, channel_num);
-                }
-            } else {
-                if (__TRACE_READ_DUMP_HEADER) {
-                    printf("%s%s Read unexpected channel num from dump header (%d)\n",
-                           indent, trace, (int) c);
-                }
             }
             break;
-        case HEADER_STATE3:
+        case 3:
             if (c == 0x01) {
-                state = HEADER_STATE4;
                 buf[bytes_read++] = c;
-
-                if (__TRACE_READ_DUMP_HEADER) {
-                    printf("%s%s Read fourth byte of dump header\n", indent, trace);
-                }
-            } else {
-                if (__TRACE_READ_DUMP_HEADER) {
-                    printf("[WARN] Read unexpected fourth byte (%02X) in dump header\n", c);
-                }
             }
             break;
-        case HEADER_STATE4:
+        case 20:
             if (c == 0xf7) {
                 done = 1;
                 buf[bytes_read++] = c;
-
-                if (__TRACE_READ_DUMP_HEADER) {
-                    printf("%s%s Read last byte of dump header\n", indent, trace);
-                    printf("%s%s Total bytes read = %d\n", indent, trace, bytes_read);
-                }
-                if (__DEBUG_READ_DUMP_HEADER) {
-                    printf("Received Dump Header ");
-                    for (i = 0; i < bytes_read; i++) {
-                        printf("%02X ", buf[i]);
-                    }
-                    printf("\n");
-                }
             } else {
-                buf[bytes_read++] = c;
+                err_set2(err, "Got %02X as the last byte of dump header (expected F7).\n", c);
+                return 0;
             }
+            break;
+        default:
+            buf[bytes_read++] = c;
             break;
         }
     }
 
-    return 1;
+    sds_serialize_header(strbuf, buf);
+    printf("Total bytes read = %d\n", bytes_read);
+    printf("Received Dump Header %s\n", strbuf);
+    return bytes_read;
 }
 
+/*
+ * We consider a read_packet that times out to mean the dump operation
+ * has completed successfully.
+ */
 static int
 read_packet(midi_t midi,
             unsigned int sample_num,
             unsigned int channel_num,
             unsigned int modded_packet_num,
             err_t err) {
-    const char *indent = "    ";
-    const char *trace = "[TRACE]";
+    const time_t timeout_sec = 1;
 
-    int done, bytes_read, i, audio_bytes_read;
-    char strbuf[500];
+    int done, i;
+    int raw_bytes_read, bytes_read, audio_bytes_read;
+    char strbuf[500]; strbuf[0] = '\0';
     unsigned char c;
     unsigned char buf[READ_PACKET_BUF_SIZE];
-    receive_packet_state_t state;
 
-    done = bytes_read = audio_bytes_read = 0;
-    state = PACKET_STATE0;
+    done = raw_bytes_read = bytes_read = audio_bytes_read = 0;
 
     printf("Reading Packet %d\n", modded_packet_num);
 
-    while (! done) {
-        if (bytes_read == READ_PACKET_BUF_SIZE) {
-            strbuf[0] = '\0';
-            sds_serialize_packet(strbuf, buf, bytes_read);
-            printf("Read Packet %d Yielded %s", modded_packet_num, strbuf);
-            printf("Maximum buffer size reached (%d)\n", READ_PACKET_BUF_SIZE);
-            break;
+    time_t start_time, now;
+    start_time = time(NULL);
+
+    while (bytes_read < SDS_PACKET_LENGTH) {
+        now = time(NULL);
+
+        if (now - start_time > timeout_sec) {
+            printf("Done!\n");
+            exit(0);
         }
 
         if (! midi_read(midi, &c)) {
-            err_set2(err, "Could not read midi");
+            err_set2(err, "Could not read midi.");
             return 0;
+        } else {
+            raw_bytes_read++;
         }
 
-        switch(state) {
-        case PACKET_STATE0:
+        switch(bytes_read) {
+        case 0:
             if (c == 0xf0) {
-                state = PACKET_STATE1;
                 buf[bytes_read++] = c;
-                
-                if (__TRACE_READ_PACKET && __TRACE_READ_PACKET_WAIT) {
-                    printf("%s%s Read first byte of packet\n", indent, trace);
-                }
             }
             break;
-        case PACKET_STATE1:
+        case 1:
             if (c == 0x7e) {
-                state = PACKET_STATE2;
                 buf[bytes_read++] = c;
-
-                if (__TRACE_READ_PACKET) {
-                    printf("%s%s Read second byte of packet\n", indent, trace);
-                }
             }
             break;
-        case PACKET_STATE2:
+        case 2:
             if (c == channel_num) {
-                state = PACKET_STATE3;
                 buf[bytes_read++] = c;
-
-                if (__TRACE_READ_PACKET) {
-                    printf("%s%s Read channel num %d from packet\n",
-                           indent, trace, channel_num);
-                }
-            } else {
-                if (__TRACE_READ_PACKET) {
-                    printf("%s%s Read unexpected channel num from packet (%d)\n",
-                           indent, trace, (int) c);
-                }
             }
             break;
-        case PACKET_STATE3:
+        case 3:
             if (c == 0x02) {
-                state = PACKET_STATE4;
                 buf[bytes_read++] = c;
-
-                if (__TRACE_READ_PACKET) {
-                    printf("%s%s Read third byte of packet\n", indent, trace);
-                }
-            } else {
-                if (__TRACE_READ_PACKET) {
-                    printf("Read unexpected byte (%02X) in packet\n", c);
-                }
             }
             break;
-        case PACKET_STATE4:
+        case 4:
             if (c == modded_packet_num) {
-                state = PACKET_STATE5;
                 buf[bytes_read++] = c;
-
-                if (__TRACE_READ_PACKET) {
-                    printf("%s%s Read fourth byte of packet\n", indent, trace);
-                }
-            } else {
-                if (__TRACE_READ_PACKET) {
-                    printf("Read %02X but expected %d in packet\n", c, modded_packet_num);
-                }
             }
             break;
-        case PACKET_STATE5:
-            buf[bytes_read++] = c;
-
-            if (audio_bytes_read < SDS_AUDIO_BYTES_PER_PACKET) {
-                audio_bytes_read++;
-            } else {
-                state = PACKET_STATE6;
-            }
-            break;
-        case PACKET_STATE6:
-            buf[bytes_read++] = c;
-
+        case 126:
             if (c == 0xf7) {
                 done = 1;
-
-                if (__TRACE_READ_PACKET) {
-                    printf("%s%s Read last byte of packet\n", indent, trace);
-                    printf("%s%s Total bytes read = %d\n", indent, trace, bytes_read);
-                }
-                if (__DEBUG_READ_PACKET) {
-                    strbuf[0] = '\0';
-                    sds_serialize_packet(strbuf, buf, bytes_read);
-                    printf("%s%s Total bytes read = %d\n", indent, trace, bytes_read);
-                    printf("Received Packet %s\n", strbuf);
-                }
+                buf[bytes_read++] = c;
             }
+            break;
+        default:
+            buf[bytes_read++] = c;
             break;
         }
     }
 
-    return 1;
+    sds_serialize_packet(strbuf, buf, bytes_read);
+    printf("Total bytes read = %d\n", bytes_read);
+    printf("Received Packet %s\n", strbuf);
+
+    return bytes_read;
 }
 
 static int
